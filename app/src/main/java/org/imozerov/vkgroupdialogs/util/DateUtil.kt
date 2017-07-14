98609ab86d39c389ebd99a9cd4041036eda3c083
Ilya Mozerov <ilyailiilia@gmail.com>
2017-07-14 21:47:40 +0300

    Added timezone to date formating

--- app/src/main/java/org/imozerov/vkgroupdialogs/util/DateUtil.kt
+++ app/src/main/java/org/imozerov/vkgroupdialogs/util/DateUtil.kt
@@ -1,14 +1,18 @@
 package org.imozerov.vkgroupdialogs.util
 
+import android.util.Log
 import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
+import java.util.*
 
 
 object DateUtil {
+    private val timeZone = TimeZone.getDefault()
+
     fun chatLastMessage(date: Long): String {
         val simpleDateFormat = SimpleDateFormat("HH:mm", Locale.ENGLISH)
-        val formattedDate = simpleDateFormat.format(date + 28800000)
+        simpleDateFormat.timeZone = timeZone
+        Log.v("ILYA", "$timeZone")
+        val formattedDate = simpleDateFormat.format(date)
         return formattedDate
     }
 }
